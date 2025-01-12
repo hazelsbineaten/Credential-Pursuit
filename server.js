f5542ed2e6e1e0d7a92594cde73f39123a1e840c
Elizabeth Yorke <157862864+hazelsbineaten@users.noreply.github.com>
2025-01-12 10:14:49 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve the start page at the root URL "/"
+
+// Start page
 app.get('/', (req, res) => {
-    console.log('Serving start.html for root URL');
     res.sendFile(path.join(__dirname, 'public', 'start.html'));
 });
 
 // Serve static files (JS, CSS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the game page for "/game"
+// Game page
 app.get('/game', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
+// Congratulations page
+app.get('/congrats.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'congrats.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
